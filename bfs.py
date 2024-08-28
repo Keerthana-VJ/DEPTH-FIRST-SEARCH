f95d5055399b74eb223b447faefa12976a550d37
Keerthana-VJ <149347704+Keerthana-VJ@users.noreply.github.com>
2024-08-28 13:39:40 +0530

    Update bfs.py

--- bfs.py
+++ bfs.py
@@ -24,14 +24,21 @@ def bfs(graph,start,visited,path):
     visited[start] = True
     while len(queue) != 0:
         tmpnode = queue.popleft()
-        #TYPE UR CODE HERE
+       
+        for neighbour in graph[tmpnode]:
+          if not visited[neighbour]:
+            path.append(neighbour)
+            queue.append(neighbour)
+            visited[neighbour]=True
     return path
 
 graph = defaultdict(list)
 v,e = map(int,input().split())
 for i in range(e):
-    #TYOE UR CODE HERE
-
+    
+    u,v=input().split()
+    graph[u].append(v)
+    graph[v].append(u)
 start = '0'
 path = []
 visited = defaultdict(bool)
